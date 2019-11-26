0e5abe6489a3f3eba761544bf3b4ce6651124d03
Larry-nan <lawrence,yinan@gmail.com>
2019-11-25 21:43:12 -0800

    add readMe & screenshot

--- README.md
+++ README.md
@@ -1,13 +1,41 @@
-# panda
-## martin
-### jimmy
-- - -
-- world
-    * power
-        - gfgffgf
-* byebye
-    - ranger
-1. love
-2. fear
-3. hate
+# Liri app
+## LIRI will be a command line node app that takes in parameters and gives you back data.
+---
+### Liri uses the following commands
+
+- Concert-this
+- Spotify-this-song
+- Movie-this
+- do-what-it says
+
+## Technologies used:
+- Node.js
+- javascript
+
+## How to Run Liri app
+
+- Users can use terminal to input the commands above to get info
+- Exapmle: node liri.js spotify-this-song <song name here>
+- This will show the following information about the song in your terminal/bash window:
+
+  * Artist(s)
+  * The song's name
+  * A preview link of the song from Spotify
+  * The album that the song is from
+
+  ## Maintain and Contributes
+  - Larry Nan
 <!-- uncomment notes -->
+Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
+Give a high-level overview of how the app is organized
+Give start-to-finish instructions on how to run the app
+Include screenshots, gifs or videos of the app functioning
+Contain a link to a deployed version of the app
+Clearly list the technologies used in the app
+State your role in the app development
+
+What the project does
+Why the project is useful
+How users can get started with the project
+Where users can get help with your project
+Who maintains and contributes to the project
--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const axios = require('axios');
 var Spotify = require('node-spotify-api');
 var moment = require('moment');
-moment().format();
+moment().format()
 
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
@@ -14,84 +14,92 @@ var spotify = new Spotify({
 
 let [, , command] = process.argv //array destructuring
 let query = process.argv.slice(3).join(" ")
-// getUserInput()
+getUserInput(command, query)
 
-// function getUserInput() {
-switch (command) {
-    case `concert-this`:
-        concertThis()
-        break;
-    case `spotify-this-song`:
-        spotifyIt()
-        break;
-    case `movie-this`:
-        movieIt()
-        break;
-    case `do-what-it-says`:
-        doIt()
-        break;
+function getUserInput(com, input) {
+    switch (com) {
+        case `concert-this`:
+            concertThis(input)
+            break;
+        case `spotify-this-song`:
+            spotifyIt(input)
+            break;
+        case `movie-this`:
+            movieIt(input)
+            break;
+        case `do-what-it-says`:
+            doIt()
+            break;
 
-    default:
-        console.log("you are no good at command line, inquirer will help") //bonus inquirer
-        break;
-}
-// }
+        default:
+            console.log("you are no good at command line, inquirer will help") //bonus inquirer
+            break;
+    }
+    // }
 
-function concertThis() {
-    const url = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp"
-    axios.get(url).then(res => {
-        console.log(res.data[0].venue.city);
-        console.log(res.data[0].venue.name);
-        console.log(res.data[0].datetime);
-    });
-}
+    function concertThis(search) {
+        // console.log(query);
+        const url = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
+        axios.get(url).then(res => {
+            console.log(`Venue location is ${res.data[0].venue.city}`);
+            console.log(`Name of the venue is ${res.data[0].venue.name}`);
+            console.log(`Date of the Event is ${moment(res.data[0].datetime).format('YYYY-MM-DD')}`)
+        });
+    }
 
-function spotifyIt() {
-    spotify.search({
-        type: 'track',
-        query, //array literal version of query:query
-        limit: 1
-    }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        console.log(`the artist is ${data.tracks.items[0].artists[0].name}`);
-        console.log(`the song's name is ${data.tracks.items[0].name}`);
-        console.log(`the preview link of the song is ${data.tracks.items[0].preview_url}`);
-        console.log(`the album is from ${data.tracks.items[0].album.name}`);
-    });
+    function spotifyIt(search) {
+        spotify.search({
+            type: 'track',
+            query: search, //array literal version of query:query
+            limit: 1
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            // console.log(data.tracks.items[0]);
 
-}
+            console.log(`the artist is ${data.tracks.items[0].artists[0].name}`);
+            console.log(`the song's name is ${data.tracks.items[0].name}`);
+            console.log(`the preview link of the song is ${data.tracks.items[0].preview_url}`);
+            console.log(`the album is from ${data.tracks.items[0].album.name}`);
+        });
 
-function movieIt() {
-    //json search for the root of the movie from OMDb
-    // `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`
-    const url = 'http://www.omdbapi.com/' +
-        `?apikey=trilogy` +
-        `&t=${query}`
+    }
 
-    axios.get(url).then(res => {
-        console.log(res.data)
-        console.log(res.data.Title);
-        console.log(res.data.Year);
-        console.log(res.data.imdbRating);
-        console.log(res.data.Country);
-        console.log(res.data.Language);
-        console.log(res.data.Plot);
-        console.log(res.data.Actors);
-    });
-}
+    function movieIt(query) {
+        //json search for the root of the movie from OMDb
+        // `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`
+        const url = 'http://www.omdbapi.com/' +
+            `?apikey=trilogy` +
+            `&t=${query}`
 
-function doIt() {
-    fs.readFile('random.txt', (err, data) => {
-        if (err) throw err;
-        // console.log(data.toString().split("\n"));
-        // console.log("doing")
-        let comArr = data.toString().split(",");
-        let com = comArr[0];
-        let com1 = comArr[1];
-        // use fs to read the file
-        // it will return a string, which we will .split(",")
-        // call getUserInput()
-    });
-}
+        axios.get(url).then(res => {
+            // console.log (`${(res.data)
+            console.log(`Title of the movie is ${res.data.Title}`);
+            console.log(`Year the movie came out ${res.data.Year}`);
+            console.log(`IMDB Rating is ${res.data.imdbRating}`);
+            console.log(`Country where the movie was produced is ${res.data.Country}`);
+            console.log(`Language is ${res.data.Language}`);
+            console.log(`Plot of the movie is ${res.data.Plot}`);
+            console.log(`Actors are ${res.data.Actors}`);
+        });
+    }
+
+    function doIt() {
+        fs.readFile('random.txt', (err, data) => {
+            if (err) throw err;
+            // console.log(data.toString().split("\n"));
+            // console.log("doing")
+            let comArr = data.toString().split(",");
+            let com = comArr[0];
+            let com1 = comArr[1];
+            // use fs to read the file
+            // it will return a string, which we will .split(",")
+            getUserInput(com, com1)
+            console.log(com);
+            console.log(com1);
+
+
+        });
+    }
+};
